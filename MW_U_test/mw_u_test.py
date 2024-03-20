2982793f9cdb10a037ad8b21873a33034f9c8399
OziomaEunice <onwubikooziomaeunice@gmail.com>
2024-03-20 21:26:08 +0000

    comment - explanation

---
+++ MW_U_test/mw_u_test.py
@@ -0,0 +1,7 @@
+# This file focuses on using a statistical significance test (SST) to determine
+# whether the mean of two samples is significantly different from each other.
+# For this case, the naive bayes, svm, and random forest are compared to each other.
+# The SST that is used is the Mann-Whitney U test.
+
+
+# Importing the necessary libraries
