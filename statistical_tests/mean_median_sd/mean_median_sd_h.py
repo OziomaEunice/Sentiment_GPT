a3bb70bcc557485145db411fc746dd0ab44a3871
OziomaEunice <onwubikooziomaeunice@gmail.com>
2024-03-26 17:56:44 +0000

    results mean, median, sd for svm, nb

--- statistical_tests/mean_median_sd/mean_median_sd_h.py
+++ statistical_tests/mean_median_sd/mean_median_sd_h.py
@@ -2,37 +2,29 @@
 import numpy as np
 
 
-# define a function to calculate mean, median and standard deviation for 
-# each perfomance metric
-def calculate_mean_median_sd(model_data, model_name):
-    for metric, values in model_data.items():
-        mean = np.mean(values)
-        median = np.median(values)
-        sd = np.std(values)
-        print(f"{model_name} ({metric}):")
-        print(f"Mean: {mean:.2f}")
-        print(f"Median: {median:.2f}")
-        print(f"Standard Deviation: {sd:.2f}")
-        print()
-
-
-
-
 #------------Twitter US Airline Sentiment Dataset (Hyperparameter)------------#
 # svm model
 svm_model_twitter_h = {
-    "accuracy": [],
-    "precision": [],
-    "recall": [],
-    "f1_score": []
+    "accuracy": [76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,
+                 76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82],
+    "precision": [0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,
+                  0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73],
+    "recall": [0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,
+               0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66],
+    "f1_score": [0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,
+                 0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68]
 }
 
 # naive bayes model
 nb_model_twitter_h = {
-    "accuracy": [],
-    "precision": [],
-    "recall": [],
-    "f1_score": []
+    "accuracy": [74.96, 74.96,74.96,74.96,74.96,74.96,74.96,74.96,74.96,74.96,
+                 74.96,74.96,74.96,74.96,74.96,74.96,74.96,74.96,74.96,74.96],
+    "precision": [0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,
+                  0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72],
+    "recall": [0.60,0.60,0.60,0.60,0.60,0.60,0.60,0.60,0.60,0.60,
+               0.60,0.60,0.60,0.60,0.60,0.60,0.60,0.60,0.60,0.60],
+    "f1_score": [0.63,0.63,0.63,0.63,0.63,0.63,0.63,0.63,0.63,0.63,
+                 0.63,0.63,0.63,0.63,0.63,0.63,0.63,0.63,0.63,0.63]
 }
 
 # random forest model
@@ -50,18 +42,26 @@ rf_model_twitter_h = {
 #------------IMDb Movie Review Dataset (Hyperparameter)------------#
 # svm model
 svm_model_imdb_h = {
-    "accuracy": [],
-    "precision": [],
-    "recall": [],
-    "f1_score": []
+    "accuracy": [57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,
+                 57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36],
+    "precision": [0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,
+                  0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49],
+    "recall": [0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,
+               0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72],
+    "f1_score": [0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,
+                 0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33]
 }
 
 # naive bayes model
 nb_model_imdb_h = {
-    "accuracy": [],
-    "precision": [],
-    "recall": [],
-    "f1_score": []
+    "accuracy": [57.41,57.41,57.41,57.41,57.41,57.41,57.41,57.41,57.41,57.41,
+                 57.41,57.41,57.41,57.41,57.41,57.41,57.41,57.41,57.41,57.41],
+    "precision": [0.47,0.47,0.47,0.47,0.47,0.47,0.47,0.47,0.47,0.47,
+                  0.47,0.47,0.47,0.47,0.47,0.47,0.47,0.47,0.47,0.47],
+    "recall": [0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,
+               0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72],
+    "f1_score": [0.34,0.34,0.34,0.34,0.34,0.34,0.34,0.34,0.34,0.34,
+                 0.34,0.34,0.34,0.34,0.34,0.34,0.34,0.34,0.34,0.34]
 }
 
 # random forest model
@@ -73,6 +73,22 @@ rf_model_imdb_h = {
 }
 
 
+
+
+# define a function to calculate mean, median and standard deviation for 
+# each perfomance metric
+def calculate_mean_median_sd(model_data, model_name):
+    for metric, values in model_data.items():
+        mean = np.mean(values)
+        median = np.median(values)
+        sd = np.std(values)
+
+        # printing outputs
+        print(f"{model_name} ({metric}):")
+        print(f"Mean: {mean:.2f}")
+        print(f"Median: {median:.2f}")
+        print(f"Standard Deviation: {sd:.2f}")
+        print()
 
 
 
--- statistical_tests/mean_median_sd/mean_median_sd_nh.py
+++ statistical_tests/mean_median_sd/mean_median_sd_nh.py
@@ -2,29 +2,17 @@
 import numpy as np
 
 
-# define a function to calculate mean, median and standard deviation for 
-# each perfomance metric
-def calculate_mean_median_sd(model_data, model_name):
-    for metric, values in model_data.items():
-        mean = np.mean(values)
-        median = np.median(values)
-        sd = np.std(values)
-        print(f"{model_name} ({metric}):")
-        print(f"Mean: {mean:.2f}")
-        print(f"Median: {median:.2f}")
-        print(f"Standard Deviation: {sd:.2f}")
-        print()
-
-
-
-
 #------------Twitter US Airline Sentiment Dataset (No Hyperparameter)------------#
 # svm model
 svm_model_twitter_nh = {
-    "accuracy": [],
-    "precision": [],
-    "recall": [],
-    "f1_score": []
+    "accuracy": [76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,
+                 76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82,76.82],
+    "precision": [0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,
+                  0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73,0.73],
+    "recall": [0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,
+               0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66,0.66],
+    "f1_score": [0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,
+                 0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68,0.68]
 }
 
 # naive bayes model
@@ -50,10 +38,14 @@ rf_model_twitter_nh = {
 #------------IMDb Movie Review Dataset (No Hyperparameter)------------#
 # svm model
 svm_model_imdb_nh = {
-    "accuracy": [],
-    "precision": [],
-    "recall": [],
-    "f1_score": []
+    "accuracy": [57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,
+                 57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36,57.36],
+    "precision": [0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,
+                  0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49,0.49],
+    "recall": [0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,
+               0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72,0.72],
+    "f1_score": [0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,
+                 0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33,0.33]
 }
 
 # naive bayes model
@@ -74,6 +66,21 @@ rf_model_imdb_nh = {
 
 
 
+
+# define a function to calculate mean, median and standard deviation for 
+# each perfomance metric
+def calculate_mean_median_sd(model_data, model_name):
+    for metric, values in model_data.items():
+        mean = np.mean(values)
+        median = np.median(values)
+        sd = np.std(values)
+
+        # printing outputs
+        print(f"{model_name} ({metric}):")
+        print(f"Mean: {mean:.2f}")
+        print(f"Median: {median:.2f}")
+        print(f"Standard Deviation: {sd:.2f}")
+        print()
 
 
 # Perform calculations for each model on the datasets
